bc97a60581fe7c9a7066d040d42c9ae5d736d92d
Connor <connor@admin.com>
2018-11-05 11:11:14 -0600

    init

---
+++ cmake-build-debug/CMakeFiles/ass5.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ass5.dir/main.c.o"
+  "ass5.pdb"
+  "ass5"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ass5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
